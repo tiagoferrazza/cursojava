d5c8dc9c734cf9d8f63475cd580d5e891c686a95
Tiago Ferrazza <tiagoferrazza@gmail.com>
2016-03-20 23:50:01 -0300

    aj1lab02

---
+++ AJ1/aj1lab02/DeclaracaoVariaveis.java
@@ -0,0 +1,12 @@
+public class DeclaracaoVariaveis{
+	public static void main(String[] args) {
+		String nome = "Tiago Ferrazza";
+		int idade = 26;
+		String rg = "5555555";
+		char sexo = 'M';
+		double salario = 1000.0;
+
+		System.out.println("Senho(a) " + nome + ", de " + idade + " anos, portador(a) do RG de número " + rg + ", do sexo " + sexo + ", esta registrado(a) com o salario de R$ " + salario);
+
+	}
+}
