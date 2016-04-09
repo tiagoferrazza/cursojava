e2ea54413d03aec4aa3133cff53cacf248633b1c
Tiago Ferrazza <tiagoferrazza@gmail.com>
2016-04-09 17:56:08 -0300

    Aula de Sabado

---
+++ AJ2/aj2lab12_01/src/main/java/br/com/globalcode/impostos/Tributavel.java
@@ -0,0 +1,7 @@
+package br.com.globalcode.impostos;
+
+public interface Tributavel {
+    public String getDescricaoTributavel();
+    public String getNomeImposto();
+    public double calcularImpostos();
+}
